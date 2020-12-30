33c2864539ed2de3711464e90e108bf8d5f66513
fuyu77 <kiwi.vc@gmail.com>
2020-12-30 15:42:13 +0900

    アロー関数に変更

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -6,7 +6,7 @@ import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
-export function getSortedPostsData () {
+export const getSortedPostsData = () => {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData = fileNames.map(fileName => {
@@ -36,7 +36,7 @@ export function getSortedPostsData () {
   })
 }
 
-export function getAllPostIds () {
+export const getAllPostIds = () => {
   const fileNames = fs.readdirSync(postsDirectory)
   return fileNames.map(fileName => {
     return {
@@ -47,7 +47,7 @@ export function getAllPostIds () {
   })
 }
 
-export async function getPostData (id: string) {
+export const getPostData = async (id: string) => {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
