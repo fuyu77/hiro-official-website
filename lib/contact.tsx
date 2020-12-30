b36e576e13766230f312968a2662348a7a9b1086
fuyu77 <kiwi.vc@gmail.com>
2020-12-30 22:45:29 +0900

    Contact追加

---
+++ lib/contact.tsx
@@ -0,0 +1,19 @@
+import remark from 'remark'
+import html from 'remark-html'
+
+interface ContactResponse {
+  markdownContent: string;
+}
+
+export const getContactData = async () => {
+  const data: ContactResponse = await fetch(
+    'https://hiro-official-website.microcms.io/api/v1/contact',
+    { headers: { 'X-API-KEY': process.env.MICROCMS_API_KEY as string } }
+  )
+    .then(response => response.json())
+  const processedContent = await remark()
+    .use(html)
+    .process(data.markdownContent)
+  const contentHtml = processedContent.toString()
+  return contentHtml
+}
---
+++ pages/contact.tsx
@@ -0,0 +1,32 @@
+import Layout from '../components/layout'
+import { getContactData } from '../lib/contact'
+import Head from 'next/head'
+import { GetStaticProps } from 'next'
+
+export default function Post ({
+  contactData
+}: {
+  contactData: string
+}) {
+  return (
+    <Layout activeTab="Contact">
+      <Head>
+        <title>Contact</title>
+      </Head>
+        <div className="hero-body container is-max-desktop">
+          <article className="content">
+            <div dangerouslySetInnerHTML={{ __html: contactData }} />
+          </article>
+        </div>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const contactData = await getContactData()
+  return {
+    props: {
+      contactData
+    }
+  }
+}
