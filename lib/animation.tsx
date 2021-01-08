916954624ddab83962df5a10e17dd06aae97a188
fuyu77 <kiwi.vc@gmail.com>
2021-01-08 22:57:21 +0900

    不要なコードを削除

--- lib/animation.tsx
+++ lib/animation.tsx
@@ -2,7 +2,6 @@ export const fadeIn = async (element: HTMLDivElement) => {
   let opacity = 0.1
   while (opacity < 1) {
     element.style.opacity = opacity.toString()
-    element.style.filter = `alpha(opacity=${opacity * 100})`
     opacity += opacity * 0.1
     await new Promise(resolve => setTimeout(resolve, 100))
   }
@@ -12,10 +11,8 @@ export const fadeOut = async (element: HTMLDivElement) => {
   let opacity = 1
   while (opacity > 0.1) {
     element.style.opacity = opacity.toString()
-    element.style.filter = `alpha(opacity=${opacity * 100})`
     opacity -= opacity * 0.1
     await new Promise(resolve => setTimeout(resolve, 100))
   }
   element.style.opacity = '0'
-  element.style.filter = 'alpha(opacity=0)'
 }
