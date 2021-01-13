4b393bb8024db5e91e5171037531f978ccdd8d74
fuyu77 <kiwi.vc@gmail.com>
2021-01-13 21:35:19 +0900

    アニメーション処理のリファクタ

--- lib/animation.tsx
+++ lib/animation.tsx
@@ -2,9 +2,9 @@ export const fadeIn = async (element: HTMLDivElement, duration: number) => {
   let opacity = 0
   const start = performance.now()
   while (opacity < 1) {
+    element.style.opacity = opacity.toString()
     const timeStamp: number = await new Promise(resolve => requestAnimationFrame(resolve))
     opacity = (timeStamp - start) / duration
-    element.style.opacity = opacity.toString()
   }
   element.style.opacity = '1'
 }
@@ -13,10 +13,10 @@ export const fadeOut = async (element: HTMLDivElement, duration: number) => {
   let opacity = 1
   const start = performance.now()
   while (opacity > 0) {
+    element.style.opacity = opacity.toString()
     const timeStamp: number = await new Promise(resolve => requestAnimationFrame(resolve))
     const easing = (timeStamp - start) / duration
     opacity = 1 - easing
-    element.style.opacity = opacity.toString()
   }
   element.style.opacity = '0'
 }
