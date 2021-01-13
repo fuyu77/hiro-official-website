4bb680d7cf1659a94c5dc2d59036b686c470667d
fuyu77 <kiwi.vc@gmail.com>
2021-01-13 21:25:01 +0900

    リファクタ

--- lib/animation.tsx
+++ lib/animation.tsx
@@ -1,11 +1,10 @@
 export const fadeIn = async (element: HTMLDivElement, duration: number) => {
   let opacity = 0
   const start = performance.now()
-  let timeStamp = start
   while (opacity < 1) {
+    const timeStamp: number = await new Promise(resolve => requestAnimationFrame(resolve))
     opacity = (timeStamp - start) / duration
     element.style.opacity = opacity.toString()
-    timeStamp = await new Promise(resolve => requestAnimationFrame(resolve))
   }
   element.style.opacity = '1'
 }
@@ -13,12 +12,11 @@ export const fadeIn = async (element: HTMLDivElement, duration: number) => {
 export const fadeOut = async (element: HTMLDivElement, duration: number) => {
   let opacity = 1
   const start = performance.now()
-  let timeStamp = start
   while (opacity > 0) {
+    const timeStamp: number = await new Promise(resolve => requestAnimationFrame(resolve))
     const easing = (timeStamp - start) / duration
     opacity = 1 - easing
     element.style.opacity = opacity.toString()
-    timeStamp = await new Promise(resolve => requestAnimationFrame(resolve))
   }
   element.style.opacity = '0'
 }
