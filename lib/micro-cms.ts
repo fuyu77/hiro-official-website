613c9e201efaadf77d0a4c53b0e45a104d896bad
fuyu77 <kiwi.vc@gmail.com>
2021-10-26 20:54:50 +0900

    Introduce X-MICROCMS-API-KEY

--- lib/micro-cms.ts
+++ lib/micro-cms.ts
@@ -1,7 +1,7 @@
 export const fetchMicroCMS = async <T>(path: string, limit = 10000): Promise<T> => {
   const root = 'https://hiro-official-website.microcms.io/api/v1/'
   const response = await fetch(`${root}${path}?limit=${limit}`, {
-    headers: { 'X-API-KEY': process.env.MICROCMS_API_KEY as string }
+    headers: { 'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY as string }
   })
   return await response.json()
 }
