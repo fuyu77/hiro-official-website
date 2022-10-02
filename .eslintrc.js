135ccf9b411af49c241d1ff536a7d9ce91c65269
fuyu77 <kiwi.vc@gmail.com>
2022-10-02 17:11:00 +0900

    npm update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2022: true
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
@@ -15,7 +15,6 @@ module.exports = {
     ecmaFeatures: {
       jsx: true
     },
-    ecmaVersion: 2021,
     sourceType: 'module',
     project: './tsconfig.json'
   },
