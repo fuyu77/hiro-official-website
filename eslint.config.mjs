1659f883c2f7b646d820a3ccd910c46077f6900a
fuyu77 <kiwi.vc@gmail.com>
2025-02-10 12:24:16 +0900

    refactor: fix indent

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,19 +3,19 @@ import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
 const compat = new FlatCompat({
-    baseDirectory: import.meta.dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 export default tseslint.config(
-    ...compat.config({
-        extends: ['next'],
-    }),
-    prettier,
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-    },
-    {
-        files: ['**/*.js', '**/*.jsx', '**/*.mjs'],
-        extends: [tseslint.configs.disableTypeChecked],
-    },
+  ...compat.config({
+    extends: ['next'],
+  }),
+  prettier,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.mjs'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
 );
