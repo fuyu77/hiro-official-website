af9f787ed24f4c2b91e5b9fd189f070f66414593
fuyu77 <kiwi.vc@gmail.com>
2026-03-11 22:49:41 +0900

    画像のスタイリングを変更

--- components/inline-item.tsx
+++ components/inline-item.tsx
@@ -1,12 +1,11 @@
 import styles from './inline-item.module.scss';
 
 interface Props {
-  readonly itemWidth?: string;
   readonly children: React.ReactNode;
 }
 
-function InlineItem({ itemWidth, children }: Props): React.ReactElement {
-  return <div className={itemWidth === 'full' ? styles.full : styles.half}>{children}</div>;
+function InlineItem({ children }: Props): React.ReactElement {
+  return <div className={styles.item}>{children}</div>;
 }
 
 export default InlineItem;
