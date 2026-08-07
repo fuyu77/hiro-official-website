7f965d4becb0ebb0ad6f04afa92e7bd8460d03b1
fuyu77 <kiwi.vc@gmail.com>
2026-08-08 07:35:47 +0900

    Biome: noNonNullAssertionに対応する
    対応ルール: style/noNonNullAssertion

--- lib/micro-cms.ts
+++ lib/micro-cms.ts
@@ -1,7 +1,11 @@
 export const fetchMicroCms = async <T>(path: string, limit = 10000): Promise<T> => {
   const root = 'https://hiro-official-website.microcms.io/api/v1/';
+  const apiKey = process.env.MICROCMS_API_KEY;
+  if (!apiKey) {
+    throw new Error('MICROCMS_API_KEY is not set');
+  }
   const response = await fetch(`${root}${path}?limit=${limit}`, {
-    headers: { 'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY! },
+    headers: { 'X-MICROCMS-API-KEY': apiKey },
   });
   return response.json() as Promise<T>;
 };
